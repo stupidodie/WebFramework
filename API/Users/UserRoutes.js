9b20e8290d26dc9b1caa5995cd8c1b39e513add8
sand <62765804+Sandrest@users.noreply.github.com>
2022-05-23 15:19:24 +0200

    Update UserRoutes.js

--- API/Users/UserRoutes.js
+++ API/Users/UserRoutes.js
@@ -12,13 +12,13 @@ import {
 
 export const userRouter = express.Router();
 const regex_email =
-  "[a-zA-Z0-9_.+]*[a-zA-Z][a-zA-Z0-9_.+]*@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$";
+  "[a-zA-Z0-9_.+]*[a-zA-Z][a-zA-Z0-9_.+]*@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+";
 userRouter.put("/user", verifyLogin);
 userRouter.post("/user", register);
-userRouter.get(`/user/:userId(${regex_email})/`, getUserInformation);
+userRouter.get(`/user/:userId(${regex_email})`, getUserInformation);
 userRouter.put(`/cart/:userId(${regex_email})`, createUserCart);
 userRouter.post(`/cart/:userId(${regex_email})`, addProduct);
-userRouter.get(`/cart/:userId(${regex_email})/`, getCart);
+userRouter.get(`/cart/:userId(${regex_email})`, getCart);
 userRouter.delete(
   `/cart/:userId(${regex_email})/products/:id`,
   deleteProductFromCart
